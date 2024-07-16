de98a2ab8bfcddb1cd9447632b8794f722272ac9
Himanshu Mourya <143244447+HIMANSHUMOURYADTU@users.noreply.github.com>
2024-07-17 00:20:32 +0530

    Create statusday1.cpp

---
+++ statusday1.cpp
@@ -0,0 +1,48 @@
+//Today I have solved three questions of ARSH DSA CHALLENGE 
+//leetcode-287(Find duplicate number)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int tortoise = nums[0];
+    int hare = nums[0];
+    
+    do {
+        tortoise = nums[tortoise];
+        hare = nums[nums[hare]];
+    } while (tortoise != hare);
+
+    // Phase 2: Finding the entrance to the cycle
+    tortoise = nums[0];
+    while (tortoise != hare) {
+        tortoise = nums[tortoise];
+        hare = nums[hare];
+    }
+
+    return hare;
+        
+    }
+};
+
+// sort colors 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+    int mid = 0;
+    int high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            std::swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        } else if (nums[mid] == 1) {
+            mid++;
+        } else { // nums[mid] == 2
+            std::swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+        
+    }
+};
